916dc9756f340a015399ba8000a1479b516816f8
793207918@qq.com <793207918@qq.com>
2017-04-14 10:14:24 +0800

    init

---
+++ src/pages/Detail/detail.vue
@@ -0,0 +1,13 @@
+<template>
+  <div id="detail">
+      <p>当前为内页</p>
+      <a href="/">首页</a>
+  </div>
+</template>
+
+<script>
+
+  export default {
+    name: 'detail',
+  };
+</script>
---
+++ src/pages/Index/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="index">
+      <p>当前为首页</p>
+      <a href="/detail.html">内页</a>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'index',
+    mounted(){
+        var a = [1,2,3]
+        console.log('index');
+        _.map(a,v=>{
+            console.log(v)
+        })
+    }
+  };
+</script>
